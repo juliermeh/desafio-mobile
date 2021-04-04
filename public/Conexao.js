2ff44b3940505f2e8ecb02d8485d61b0de01c1c3
Julierme Jadon <julierme.jadon@outlook.com>
2021-04-04 11:30:46 -0300

    Conexao com a API

--- public/Conexao.js
+++ public/Conexao.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 import md5 from 'md5';
 
 const baseURL = 'http://gateway.marvel.com/v1/public/comics?';
-const publicKey = '';
-const privateKey = '';
+const publicKey = 'd2446f098f63d08b4988838e8f900060';
+const privateKey = 'b380902cad5dd109768db104d10a5661d3c6a3fc';
 const ts = Number(new Date());
-const hash_marvel = md5(ts + privateKey + publicKey);
+const hash = md5(ts + privateKey + publicKey);
 
 const Conexao: React.FC = () => {
-    useEffect(() => {},[]);
+    useEffect(() => {
+        axios.get(baseURL+'ts=${ts}&apikey=${publicKey}&hash=${hash}')
+            .then(response => console.log(response))
+            .catch(erro => console.log(erro))
+    },[]);
     return null;
 }
--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import md5 from 'md5';
+
+const baseURL = 'http://gateway.marvel.com/v1/public/comics?';
+const publicKey = '';
+const privateKey = '';
+const ts = Number(new Date());
+const hash = md5(ts + privateKey + publicKey);
+
+const Conexao: React.FC = () => {
+  useEffect(() => {
+    axios.get(baseURL+'ts=${ts}&apikey=${publicKey}&hash=${hash}')
+        .then(response => console.log(response))
+        .catch(erro => console.log(erro))
+  },[]);
+  return null;
+}
 
 function App() {
+  const conexao = Conexao();
   return ("index.html");
 }
 
